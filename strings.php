163e570bddcee695120cf7dc91e31fd16c5b3ca6
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-20 16:51:53 +0900

    Escape Sequence

--- strings.php
+++ strings.php
@@ -1,8 +1,15 @@
 <?php
 $name = 'Alena';
-$string_one = "Hello?? $name";
-echo $string_one;
-?>
+$string_one = "Hello?? \$name";
+// Hello?? $name
+
+$string_two = "Hello?? \"$name\" ";
+// Hello?? "Alena"
+
+echo $string_two;
 
 // If you want to use var in string,
 // You have to use double quote instead single.
+?>
+
+
