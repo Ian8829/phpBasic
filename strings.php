d2fee7cab66f0b7faf7bd9e8ea57043a79ef14fc
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-20 17:46:39 +0900

    Comparing Values

--- strings.php
+++ strings.php
@@ -1,20 +1,10 @@
 <?php
-$name = 'Alena';
-$string_one = 'Combining multiple ' .$name. '???';
-$string_two = 'Learning to display ';
-$string_two .= '"Hello ';
-$string_two .= $name;
-$string_two .= '!" to the screen.';
-$string_two .= "\n";
-// $string_one = $string_one . "\n";
 
-echo $string_one;
-
-$isReady = true;
-var_dump($isReady);
-$isReady = false;
-var_dump($isReady);
-
+$a = 10;
+$b = '10';
+var_dump($a == $b);
+var_dump($a === $b);
+var_dump($string_one === 'Learning to display "Hello" to the screen.');
 ?>
 
 
