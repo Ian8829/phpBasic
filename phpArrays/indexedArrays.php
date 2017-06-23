a247f1b92274266654b5a57430b317c20a5a47eb
taeshik Hwang <ghjkghj@naver.com>
2017-06-23 18:40:40 +0900

    Adding Array Elements

--- phpArrays/indexedArrays.php
+++ phpArrays/indexedArrays.php
@@ -1,6 +1,16 @@
 <?php
 $learn = array('Conditionals', 'Arrays', 'Loops');
-//var_dump($learn);
+
+$learn[] = 'Build something awesome!';
+//$learn = 'Build something awesome!';
+
+array_push($learn, 'Functions', 'Forms', 'Objects');
+// from end of the array
+
+array_unshift($learn, 'HTML', 'CSS');
+// from first of the array
+
+var_dump($learn);
 //echo $learn[0];
 //echo $learn;
-echo implode("\n", $learn);
+//echo implode("\n", $learn);
