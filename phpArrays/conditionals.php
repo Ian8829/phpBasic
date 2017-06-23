ac1e9ca3bd9d47dbf91aaf50124d9ff259d45840
taeshik Hwang <ghjkghj@naver.com>
2017-06-23 16:40:41 +0900

    Nested Conditional Statements

--- phpArrays/conditionals.php
+++ phpArrays/conditionals.php
@@ -1,7 +1,19 @@
 <?php
+$num = 1;
+
+if ($num >= 10) {
+    if ($num <= 1000) {
+        echo 'your number is within the range';
+    } else {
+        echo 'your number is greater than 1000, NOT within the range';
+    }
+} else {
+    echo ' your number is less than 10, NOT within the range';
+}
+
+/*
 $a = 0;
 $b = 8;
-
 
 if ($a == $b) {
     echo 'values are equal ';
@@ -42,6 +54,8 @@ if ($a == false) {
 if ($a != true) {
     echo ' false';
 }
+*/
+
 
 ?>
 
