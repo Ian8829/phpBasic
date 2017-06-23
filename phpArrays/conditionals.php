77a356eec134c9874ff46ce25722ea16910685ef
taeshik Hwang <ghjkghj@naver.com>
2017-06-23 16:12:43 +0900

    Comparison Operators

---
+++ phpArrays/conditionals.php
@@ -0,0 +1,22 @@
+<?php
+$a = 5;
+$b = 8;
+
+if ($a == $b) {
+    echo ' values are equal ';
+} elseif ($a < $b) {
+    echo ' $a is less than $b';
+} elseif ($a > $b) {
+    echo ' $a is greater than $b';
+} else {
+    echo ' values are NOT equal ';
+}
+
+$score = 57;
+
+if ($score >= 59) {
+    echo 'You completed the level1';
+} else {
+    echo 'Please try again';
+}
+?>
