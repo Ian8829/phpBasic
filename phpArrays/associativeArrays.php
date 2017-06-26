f3567174eb68a4db2cb151d8b2959b5df412f05c
taeshik Hwang <ghjkghj@naver.com>
2017-06-26 10:27:22 +0900

    Associative Arrays

---
+++ phpArrays/associativeArrays.php
@@ -0,0 +1,10 @@
+<?php
+$iceCream = array(
+    'Alena' => 'Black Cherry',
+    'Treasure' => 'Chocolate',
+    'Dave' => 'Cookies and Cream',
+    'Rialla' => 'Strawberry'
+);
+
+var_dump($iceCream);
+//echo $iceCream['Alena'];
