78e678e6af49d2ef14703f80373c85ba47a515e1
taeshik Hwang <ghjkghj@naver.com>
2017-06-26 10:45:27 +0900

    Mixing Data Types in Arrays

--- phpArrays/associativeArrays.php
+++ phpArrays/associativeArrays.php
@@ -2,9 +2,21 @@
 $iceCream = array(
     'Alena' => 'Black Cherry',
     'Treasure' => 'Chocolate',
-    'Dave' => 'Cookies and Cream',
+    'Dave MacFarland' => 'Cookies and Cream',
     'Rialla' => 'Strawberry'
 );
 
+$iceCream['alena'] = 'Pistachio';
+$iceCream['Dave Thomas'] = 'Cookie Dough';
+$iceCream[] = 'Vanilla';
+$iceCream['Andrew'] = true;
 var_dump($iceCream);
-//echo $iceCream['Alena'];
+
+$keys = [
+  1 => 'a',
+  '1' => 'b',
+  1.5 => 'c',
+  true => 'd'
+];
+
+var_dump($keys);
