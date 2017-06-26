5d1ef24cf617e2c4f9d33b741005b0d9df5514a5
taeshik Hwang <ghjkghj@naver.com>
2017-06-26 16:03:58 +0900

    Function Default Arguments3

--- phpFunctions/index.php
+++ phpFunctions/index.php
@@ -1,7 +1,11 @@
 <?php
 
-function getInfo($name, $title = 'friend') {
-  echo "$name has arrived, they are with us as a $title";
+function getInfo($name, $title = Null) {
+  if ($title) {
+    echo "$name has arrived, they are with us as a $title";
+  } else {
+    echo "$name has arrived, welcome!!";
+  }
 }
 
 //getInfo('Mike', 'frog');
